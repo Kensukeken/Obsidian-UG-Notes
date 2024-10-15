a0f898528dfc2c495433c3f0a1a03ec34965a6cd
Kenzy <85637598+Kensukeken@users.noreply.github.com>
2024-10-14 23:48:47 -0400

    Create lab1.cpp

---
+++ Elementary Programming Concepts/Labs/lab1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+    /* Create a program that does the following:
+    *  Outputs to the screen the following:
+    *  Your name
+    *  A description about yourself
+    *  Why you picked the program
+    * */
+    std::cout << "Kensukeken" << std::endl;
+    std::cout << "I'm a painter, a programmer, and a lifelong learner." << std::endl;
+    return 0;
+}
