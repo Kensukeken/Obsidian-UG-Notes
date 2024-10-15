6b2e1b2fe64dbe3edec70e16fa75cfa8fcecfda9
Kenzy <85637598+Kensukeken@users.noreply.github.com>
2024-10-14 23:49:09 -0400

    Create lab2.cpp

---
+++ Elementary Programming Concepts/Labs/lab2.cpp
@@ -0,0 +1,10 @@
+public class lab2 {
+    public static void main(String[] args) {
+        int firstNum = 2;
+        int secondNum = 4;
+        int thirdNum = 6;
+
+        double average = (firstNum+secondNum+thirdNum)/3;
+        System.out.println(average);
+    }
+}
