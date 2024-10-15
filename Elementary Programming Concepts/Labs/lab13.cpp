345789cd7b91ddc1bbd0e3111cd129afff4cbae6
Kenzy <85637598+Kensukeken@users.noreply.github.com>
2024-10-14 23:57:40 -0400

    Create lab13.cpp

---
+++ Elementary Programming Concepts/Labs/lab13.cpp
@@ -0,0 +1,14 @@
+public class lab13 {
+    public static void displayPattern(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = i; j >= 1; j--) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        displayPattern(5); 
+    }
+}
