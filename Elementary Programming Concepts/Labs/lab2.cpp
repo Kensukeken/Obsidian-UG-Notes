577f96f1e32a5557e086cf5fe3ec3c07ee573765
Kenzy <85637598+Kensukeken@users.noreply.github.com>
2024-10-14 23:49:46 -0400

    Update lab2.cpp

--- Elementary Programming Concepts/Labs/lab2.cpp
+++ Elementary Programming Concepts/Labs/lab2.cpp
@@ -1,10 +1,12 @@
-public class lab2 {
-    public static void main(String[] args) {
-        int firstNum = 2;
-        int secondNum = 4;
-        int thirdNum = 6;
+#include <iostream>
 
-        double average = (firstNum+secondNum+thirdNum)/3;
-        System.out.println(average);
-    }
+int main() {
+    int firstNum = 2;
+    int secondNum = 4;
+    int thirdNum = 6;
+
+    double average = (firstNum + secondNum + thirdNum) / 3.0;
+    std::cout << average << std::endl;
+
+    return 0;
 }
